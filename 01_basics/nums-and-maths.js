4c8b5def07f1f816c7f4be3f1655b12c0faef53e
animesh <phb21013@tezu.ac.in>
2023-12-25 14:47:57 +0530

    Update nums-and-maths.js

--- 01_basics/nums-and-maths.js
+++ 01_basics/nums-and-maths.js
@@ -1,35 +1,35 @@
 const score = 400  
-// console.log(score);
+console.log(score);
 
 const balance = new Number(100)   
-// console.log(balance); //type is shown 
+console.log(balance); //type is shown 
 
-// console.log(balance.toString());  //number converted to string
-// console.log(balance.toString().length);   
-// console.log(balance.toFixed(3));   // want 3 numbres after decimal , used for ecoomerce cite 
+console.log(balance.toString());  //number converted to string
+console.log(balance.toString().length);   
+console.log(balance.toFixed(3));   // want 3 numbres after decimal , used for ecoomerce cite 
 
 const otherNumber = 423.89399
 
-// console.log(otherNumber.toPrecision(3));    // to get precise value and here ans is 424
+console.log(otherNumber.toPrecision(3));    // to get precise value and here ans is 424
 
 const newNum = 100000
-// console.log(newNum.toLocaleString('en-IN'));  //To convert the counting to local sysytem like indian : comas and all
+console.log(newNum.toLocaleString('en-IN'));  //To convert the counting to local sysytem like indian : comas and all
 
 
 
 //+==============+MATHS+==============+
 
-// console.log(Math);
-// console.log(Math.abs(-4));  //change negative value to positive
-// console.log(Math.round(4.6));  //round of : ans is 5
-// console.log(Math.ceil(4.5)); // the highest value will be print 
-// console.log(Math.floor(4.5));  //lowest round of is going to happpen
-// console.log(Math.min(1, 2, 3, 5));  //get the min value 
-// console.log(Math.max(1, 2, 3, 5)); // get the max valaue 
+console.log(Math);
+console.log(Math.abs(-4));  //change negative value to positive
+console.log(Math.round(4.6));  //round of : ans is 5
+console.log(Math.ceil(4.5)); // the highest value will be print 
+console.log(Math.floor(4.5));  //lowest round of is going to happpen
+console.log(Math.min(1, 2, 3, 5));  //get the min value 
+console.log(Math.max(1, 2, 3, 5)); // get the max valaue 
 
 
-// console.log(Math.random());
-// console.log((Math.random()*10) + 1);
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) +1);
 
 
