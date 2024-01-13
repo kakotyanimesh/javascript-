8813a64078eaa64a5cdf030aa70bd6040c9bb832
animesh <phb21013@tezu.ac.in>
2024-01-13 23:03:56 +0530

    scope update

---
+++ 03_basics/scope.js
@@ -0,0 +1,23 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// why using var 
+
+
+// {}  this are scopes when it use with function and if else and values outside the scope i,e outside the {} are known as global 
+
+
+if (true){
+    let a = 10
+    const b = 20
+    var c = 30
+
+}
+ // here we can print var outside the {} but not the let and const value so we can access var outside the {} 
+//  and if we add var d = 30 and if (){var d = 69}
+//   then while printing d the value changes to d 
+
+// console.log(a);
+// console.log(b);
+console.log(c);
