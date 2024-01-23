0251f25d739a394cce726ffa87d14b9244e4d58f
animesh <phb21013@tezu.ac.in>
2024-01-23 20:49:26 +0530

    iife.js

---
+++ 03_basics/iife.js
@@ -0,0 +1,25 @@
+// immediately invoked function expression {iife}
+
+// global scope kei pollution sei bachne ke liye iife use kiya 
+// and for imediate execution 
+//          syntax : ()() 
+
+(function (){
+    console.log(`iife execution`);
+})();
+
+(function namedIifee(){
+    // its a named iife
+    console.log(`named iife execution `);
+})();
+
+// dont forget to put ; to end the iife 
+
+(() => {
+    console.log(`iife execution in arrow funtion`);
+})();     // dont forget to end 
+
+
+((name) => {
+    console.log(`iife execution object ${name}`);
+})('anieh')
