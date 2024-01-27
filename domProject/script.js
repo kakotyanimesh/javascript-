f8594c1d626c41c79925a8f64a35728e462fde15
animesh <phb21013@tezu.ac.in>
2024-01-27 21:00:29 +0530

    dom project basic

---
+++ domProject/script.js
@@ -0,0 +1,7 @@
+
+function populateDiv(){
+    const a = document.getElementById("firstNumber").value;                 // .value to get the value from user 
+    const b = document.getElementById("secondNumber").value;
+    const sum = document.getElementById("finalSum")
+    sum.innerHTML = parseInt(a) + parseInt(b)                             // parse int to convert to number and innerHTML to add text 
+}
