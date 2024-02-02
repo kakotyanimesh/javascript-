b5d69facf3e67d00a6b77193814cc100fa695ffb
animesh <phb21013@tezu.ac.in>
2024-02-02 13:52:16 +0530

    done

--- react/animesh/src/App.jsx
+++ react/animesh/src/App.jsx
@@ -1,18 +1,62 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div>
+      
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+        
+//     </div>
+//   )
+// }
+
+// export default App
+
+// let state = {
+//   count : 0
+// }
+
+// function App() {
+//   function onClickHandeler() {
+//     // alert('animeh')
+//     state.count = state.count + 1;
+
+//   }
+//   return (
+//     <div>
+//       <button onClick={onClickHandeler} >counter {state.count}</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+function App(){
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
+      <CustomButon count={count} setCount={setCount} ></CustomButon>
     </div>
   )
+}
+
+//component 
+
+function CustomButon(props) {
+
+  function onClickHandeler() {
+    props.setCount(props.count + 1)
+
+  }
+  return <button onClick={onClickHandeler}>counter {props.count}</button>
+  
 }
 
 export default App
