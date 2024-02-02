7ade7b642d13bcf566979f29c2e20499e452a0f6
animesh <phb21013@tezu.ac.in>
2024-02-02 15:46:56 +0530

    todo app

--- react/animesh/src/App.jsx
+++ react/animesh/src/App.jsx
@@ -36,26 +36,91 @@ import './App.css'
 
 // export default App
 
+                         // counter app 
+// function App(){
+//   const [count, setCount] = useState(0)
 
-function App(){
-  const [count, setCount] = useState(0)
+//   return (
+//     <div>
+//       <CustomButon count={count} setCount={setCount} ></CustomButon>
+//     </div>
+//   )
+// }
 
-  return (
+// //component 
+
+// function CustomButon(props) {
+
+//   function onClickHandeler() {
+//     props.setCount(props.count + 1)
+
+//   }
+//   return <button onClick={onClickHandeler}>counter {props.count}</button>
+  
+// }
+
+// export default App
+
+
+// counter app done now lets make a todo app 
+
+
+function App() {
+                                                    // have to write under array []
+  const [todos, setTodos] = useState([{
+    title : 'go to gym', 
+    description: 'go to gym by 9 to 10',
+    completed : false
+   }, {
+    title : 'go to study',
+    description: 'go to gym by 10 to 3pm',
+    completed : false
+   }])
+
+
+  
+   return (
     <div>
-      <CustomButon count={count} setCount={setCount} ></CustomButon>
+      {/* add a button */}
+      
+
+      <button onClick={()=>{
+        setTodos ([...todos, {
+          title: 'newtodos',
+          description: 'fuckedup'
+        }])
+      }}>add todo </button>
+
+      {/* <Todo title={todos[0].title} description={todos[0].description}/>
+      <Todo title={todos[1].title} description={todos[1].description}/>
+       */}
+       
+      {/* we can do the sama thing by using the map  */}
+
+
+      {todos.map(function (todos) {
+        return <Todo title={todos.title} description={todos.description}/>
+        
+      })}
+
+
+
+       
+       {/* {todos.map(function (todos) {
+        return <Todo title= {todos.title} description={todos.description}/>
+       })} */}
     </div>
-  )
+   )
+  
 }
 
-//component 
+// lets write the component 
 
-function CustomButon(props) {
-
-  function onClickHandeler() {
-    props.setCount(props.count + 1)
-
-  }
-  return <button onClick={onClickHandeler}>counter {props.count}</button>
+function Todo(para) {
+  return <div>
+    <h1>{para.title}</h1>
+    <h2>{para.description}</h2>
+  </div>
   
 }
 
