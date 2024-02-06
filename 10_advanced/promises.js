18a62c3709e6a9d7c6dc453c40cd2fdc3883c041
animesh <phb21013@tezu.ac.in>
2024-02-06 15:02:19 +0530

    api calls and promises
    api calls and basics of promises

---
+++ 10_advanced/promises.js
@@ -0,0 +1,74 @@
+const promiseOne = new Promise((resolve, reject)=>{
+    // do an async task 
+    // db calls, cryptography, network calls
+    setTimeout(() => {
+        console.log('async task is completed');
+        resolve()                // connecting to resolve 
+    }, 1000);
+
+})
+
+// resolve has connection with .then
+promiseOne.then(()=>{
+    console.log('promise consumed');
+
+})                
+
+
+
+new Promise((resolve, reject)=>{
+    setTimeout(() => {
+        console.log('async task two');
+        resolve()
+    }, 1000);
+}).then(()=>{
+    console.log('async two resolved');
+})
+
+
+// third promise
+
+const promiseThree = new Promise((resolve, reject)=>{
+    setTimeout(() => {
+        resolve({username: "animesh", email: "animeshkakoty33@gmail.com"})
+    }, 1000);
+})
+
+promiseThree.then((useer)=>{
+console.log(useer);
+
+})
+
+
+
+// 4th promise
+
+const promiseFourth = new Promise((resolve, reject)=>{
+    setTimeout(() => {
+        let error = true
+        if (!error) {
+            resolve({username: "animesh", password: "123" })
+        } else{
+            reject('ERROR: something went wrong')
+        }
+    }, 1000);
+
+})
+
+promiseFourth
+.then((user)=>{
+    console.log(user);
+    return user.username
+
+})
+.then((myusername)=>{
+console.log(myusername);
+})
+.catch((err)=>{
+console.log(err);
+})
+.finally(()=>{
+    console.log('the promise is either resolved or rejected');
+})
+
+// 31.49
