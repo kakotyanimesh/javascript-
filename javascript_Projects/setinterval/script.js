5a4dbc0ddeb63c60df5baeb591047477c01bc57a
animesh <phb21013@tezu.ac.in>
2024-02-26 16:34:07 +0530

    js

---
+++ javascript_Projects/setinterval/script.js
@@ -0,0 +1,29 @@
+const alphabets = document.getElementById("alphabets")
+const stopbtn = document.getElementById("stop")
+const startbtn = document.getElementById("start")
+
+let letters = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
+
+let i = 0 
+function updateAlphabets() {
+    alphabets.textContent = letters
+    [i];
+    i++;
+    if (i === 25) {
+        i = 0
+    }
+
+
+}
+
+
+
+startbtn.addEventListener('click',()=>{
+    
+ interval =  setInterval(updateAlphabets, 100)
+})
+
+stopbtn.addEventListener('click', ()=>{
+    clearInterval(interval)
+}
+)
