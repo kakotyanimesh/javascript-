8da72988126698487656c50d90ae8d13fe1e4e04
animesh kakoty <96949970+kakotyanimesh@users.noreply.github.com>
2024-09-27 02:23:01 +0530

    Update db.js

--- ReactProject/backend/db.js
+++ ReactProject/backend/db.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose")
 const { string, boolean } = require('zod')
 
 // mongodb+srv://animeshkakoty07:kakoty69@cluster0.zjl81yu.mongodb.net/
-mongoose.connect("mongodb+srv://animeshkakoty07:kakoty69@cluster0.zjl81yu.mongodb.net/")
+mongoose.connect("")
 const todoSchema = mongoose.Schema({
     title: String,
     description: String,
